80c470ba858692f9a85d2b9dde2827476a62b9c4
olegm <olegmahmutov908@gmail.com>
2025-03-04 15:57:46 +0300

    11.1 update#2

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,45 +1,38 @@
-import pytest
+import unittest
 from src import masks
+from tests.conftest import short_card_number, long_card_number, account_number, empty_account_number, \
+    card_number_and_expected
 
 
-@pytest.fixture
-def biblioteka():
-    return "1234567890123456"
+def test_mask_card_number_valid(card_number_and_expected):
+    """Тест маски номера карты с использованием параметризации."""
+    card_number, expected_output = card_number_and_expected
+    assert masks.mask_card_number(card_number) == expected_output
 
 
-class TestMasking:
+def test_mask_card_number_short(short_card_number):
+    """Тест короткой маски номера карты."""
+    expected_output = "1234 56** **** 34"
+    assert masks.mask_card_number(short_card_number) == expected_output
 
-    def test_mask_card_number_valid(self, biblioteka):
-        """Тест маски номера карты"""
-        expected_output = "1234 56** **** 3456"
-        assert masks.mask_card_number(biblioteka) == expected_output
 
-    def test_mask_card_number_short(self):
-        """Тест короткой маски номера карты"""
-        card_number = "12345678901234"
-        expected_output = "1234 56** **** 34"
-        assert masks.mask_card_number(card_number) == expected_output
+def test_mask_card_number_long(long_card_number):
+    """Тест длинной маски номера карты."""
+    expected_output = "1234 56** **** 34567890"
+    assert masks.mask_card_number(long_card_number) == expected_output
 
-    def test_mask_card_number_long(self):
-        """Тест длинной маски номера карты"""
-        card_number = "12345678901234567890"
-        expected_output = "1234 56** **** 34567890"
-        assert masks.mask_card_number(card_number) == expected_output
 
-    def test_mask_account_number_valid(self):
-        """Тест маски номера счета"""
-        account_number = "12345678"
-        expected_output = "** 5678"
-        assert masks.mask_account_number(account_number) == expected_output
+def test_mask_account_number_valid(account_number):
+    """Тест маски номера счета."""
+    expected_output = "** 5678"
+    assert masks.mask_account_number(account_number) == expected_output
 
-    def test_mask_account_number_short(self):
-        """Тест короткой маски номера счета"""
-        account_number = "** 123"
-        expected_output = "**  123"
-        assert masks.mask_account_number(account_number) == expected_output
 
-    def test_mask_account_number_exact_length(self):
-        """Тест точной длины маски номера счета"""
-        account_number = "1234"
-        expected_output = "** 1234"
-        assert masks.mask_account_number(account_number) == expected_output
+def test_mask_account_number_empty(empty_account_number):
+    """Тест пустой маски номера счета."""
+    expected_output = "** "
+    assert masks.mask_account_number(empty_account_number) == expected_output
+
+
+if __name__ == "__main__":
+    unittest.main()
